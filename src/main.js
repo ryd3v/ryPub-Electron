c5102e97c9b7d79dd2a983f50981a3f6047929ef
ryd3v <hello@ryd3v.com>
2023-08-05 01:58:33 -0400

    Fix TOC render

--- src/main.js
+++ src/main.js
@@ -33,69 +33,70 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
 
-const menuTemplate = [
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Open',
-                click: function () {
-                    dialog.showOpenDialog(mainWindow, {
-                        properties: ['openFile'],
-                        filters: [{name: 'EPUB', extensions: ['epub']}]
-                    }).then(file => {
-                        if (!file.canceled) {
-                            mainWindow.webContents.send('file-opened', path.resolve(file.filePaths[0]))
-                        }
-                    }).catch(err => {
-                        console.log(err)
-                    })
+const generateMenu = () => {
+    return [
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Open',
+                    click: function () {
+                        dialog.showOpenDialog(mainWindow, {
+                            properties: ['openFile'],
+                            filters: [{name: 'EPUB', extensions: ['epub']}]
+                        }).then(file => {
+                            if (!file.canceled) {
+                                mainWindow.webContents.send('file-opened', path.resolve(file.filePaths[0]))
+                            }
+                        }).catch(err => {
+                            console.log(err)
+                        })
+                    }
+                },
+                {
+                    label: 'Table of Contents',
+                    submenu: tocMenu
+                },
+                {
+                    label: 'Close',
+                    click: () => {
+                        mainWindow.webContents.send('file-closed')
+                    }
+                },
+                {type: 'separator'},
+                {
+                    label: 'Exit',
+                    click: function () {
+                        app.quit()
+                    }
                 }
-            },
-            {
-                label: 'Table of Contents',
-                submenu: tocMenu
-            },
-            {
-                label: 'Close',
-                click: () => {
-                    mainWindow.webContents.send('file-closed')
+            ]
+        },
+        {
+            label: 'About',
+            submenu: [
+                {
+                    label: 'About',
+                    click: function () {
+                        let aboutWindow = new BrowserWindow({
+                            width: 400,
+                            height: 400,
+                            webPreferences: {
+                                nodeIntegration: true,
+                                contextIsolation: false
+                            }
+                        })
+                        aboutWindow.loadFile('src/about.html').then(() => {
+                            aboutWindow.webContents.send('app_version', version);
+                        })
+                    }
                 }
-            },
-            {type: 'separator'},
-            {
-                label: 'Exit',
-                click: function () {
-                    app.quit()
-                }
-            }
-        ]
-    },
-    {
-        label: 'About',
-        submenu: [
-            {
-                label: 'About',
-                click: function () {
-                    let aboutWindow = new BrowserWindow({
-                        width: 400,
-                        height: 400,
-                        webPreferences: {
-                            nodeIntegration: true,
-                            contextIsolation: false
-                        }
-                    })
-                    aboutWindow.loadFile('src/about.html').then(() => {
-                        aboutWindow.webContents.send('app_version', version);
-                    })
-                }
-            }
-        ]
-    }
-]
+            ]
+        }
+    ]
+};
 
-
-Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate))
+Menu.setApplicationMenu(Menu.buildFromTemplate(generateMenu()));
 
 ipcMain.on('toc-ready', (event, toc) => {
     tocMenu = toc.map(tocItem => {
@@ -107,16 +108,6 @@ ipcMain.on('toc-ready', (event, toc) => {
         };
     });
 
-    // console.log('TOC Menu before building:', tocMenu);
-    const tocIndex = menuTemplate.findIndex(item => item.label === 'File');
-    if (tocIndex > -1) {
-        const tocSubmenuIndex = menuTemplate[tocIndex].submenu.findIndex(item => item.label === 'TOC');
-        if (tocSubmenuIndex > -1) {
-            menuTemplate[tocIndex].submenu[tocSubmenuIndex].submenu = tocMenu;
-        }
-    }
-
-    // console.log('Updated menuTemplate:', JSON.stringify(menuTemplate, null, 2));
-    const menu = Menu.buildFromTemplate(menuTemplate);
+    const menu = Menu.buildFromTemplate(generateMenu());
     Menu.setApplicationMenu(menu);
 });
