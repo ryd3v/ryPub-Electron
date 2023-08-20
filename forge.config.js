400c139c15f45cdea8f76224abbe7f274153ff6a
ryd3v <hello@ryd3v.com>
2023-08-20 16:58:48 -0400

    Update Icon

--- forge.config.js
+++ forge.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-  packagerConfig: {
-    icon: 'src/images/book-icon',
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+    packagerConfig: {
+        icon: 'src/images/book',
+        asar: true,
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {
-        options: {
-          icon: 'src/images/book-icon.png',
-          category: "Utilities",
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {},
         },
-      },
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {
+                options: {
+                    icon: 'src/images/book.png',
+                    category: "Utilities",
+                },
+            },
+        },
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-auto-unpack-natives',
+            config: {},
+        },
+    ],
 };
