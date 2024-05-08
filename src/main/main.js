b437531ff4d427d3a0d96bc284b022053b6b983a
ryd3v <hello@ryd3v.com>
2024-05-08 04:05:03 -0400

    remove electron-store

--- src/main/main.js
+++ src/main/main.js
@@ -1,146 +1,141 @@
-const {app, BrowserWindow, dialog, Menu, ipcMain} = require('electron')
-const path = require('path')
-const {version} = require('../../package.json');
-const Store = require('electron-store');
+const { app, BrowserWindow, dialog, Menu, ipcMain } = require('electron');
+const path = require('path');
+const { version } = require('../../package.json');
 
-const store = new Store();
-
-ipcMain.on('electron-store-get-data', (event, arg) => {
-    if (arg && typeof arg.key !== 'undefined') {
-        event.returnValue = store.get(arg.key, arg.defaultValue);
-    } else {
-        event.returnValue = null;
-    }
-});
-
-let mainWindow
-let tocMenu = []
+let mainWindow;
+let tocMenu = [];
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 600,
-        height: 900,
-        icon: "images/book.png",
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    })
-    mainWindow.loadFile('src/html/index.html')
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 900,
+    icon: 'images/book.png',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadFile('src/html/index.html');
 }
 
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    });
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
 const generateMenu = () => {
-    return [
+  return [
+    {
+      label: 'File',
+      submenu: [
         {
-            label: 'File',
-            submenu: [
-                {
-                    label: 'Open',
-                    click: function () {
-                        dialog.showOpenDialog(mainWindow, {
-                            properties: ['openFile'],
-                            filters: [{name: 'EPUB', extensions: ['epub']}]
-                        }).then(file => {
-                            if (!file.canceled) {
-                                mainWindow.webContents.send('file-opened', path.resolve(file.filePaths[0]))
-                            }
-                        }).catch(err => {
-                            console.log(err)
-                        })
-                    }
-                },
-                {
-                    label: 'Table of Contents',
-                    submenu: tocMenu
-                },
-                {
-                    label: 'Close',
-                    click: () => {
-                        mainWindow.webContents.send('file-closed')
-                    }
-                },
-                {type: 'separator'},
-                {
-                    label: 'Exit',
-                    click: function () {
-                        app.quit()
-                    }
+          label: 'Open',
+          click: function () {
+            dialog
+              .showOpenDialog(mainWindow, {
+                properties: ['openFile'],
+                filters: [{ name: 'EPUB', extensions: ['epub'] }],
+              })
+              .then((file) => {
+                if (!file.canceled) {
+                  mainWindow.webContents.send(
+                    'file-opened',
+                    path.resolve(file.filePaths[0])
+                  );
                 }
-            ]
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          },
         },
         {
-            label: 'View',
-            submenu: [
-                {
-                    label: '🔆 Dark Mode',
-                    click: function () {
-                        mainWindow.webContents.send('toggle-dark-mode');
-                    }
-                }
-            ]
+          label: 'Table of Contents',
+          submenu: tocMenu,
         },
         {
-            label: 'About',
-            submenu: [
-                {
-                    label: 'About',
-                    click: function () {
-                        let aboutWindow = new BrowserWindow({
-                            width: 500,
-                            height: 500,
-                            icon: "src/images/book-icon.png",
-                            webPreferences: {
-                                nodeIntegration: true,
-                                contextIsolation: false
-                            }
-                        })
-                        aboutWindow.loadFile('src/html/about.html').then(() => {
-                            aboutWindow.webContents.send('app_version', version);
-                        })
-                    }
-                }
-            ]
-        }
-    ]
+          label: 'Close',
+          click: () => {
+            mainWindow.webContents.send('file-closed');
+          },
+        },
+        { type: 'separator' },
+        {
+          label: 'Exit',
+          click: function () {
+            app.quit();
+          },
+        },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: '🔆 Dark Mode',
+          click: function () {
+            mainWindow.webContents.send('toggle-dark-mode');
+          },
+        },
+      ],
+    },
+    {
+      label: 'About',
+      submenu: [
+        {
+          label: 'About',
+          click: function () {
+            let aboutWindow = new BrowserWindow({
+              width: 500,
+              height: 500,
+              icon: 'src/images/book-icon.png',
+              webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+              },
+            });
+            aboutWindow.loadFile('src/html/about.html').then(() => {
+              aboutWindow.webContents.send('app_version', version);
+            });
+          },
+        },
+      ],
+    },
+  ];
 };
 
 Menu.setApplicationMenu(Menu.buildFromTemplate(generateMenu()));
 
 ipcMain.on('toc-ready', (event, toc) => {
-    tocMenu = toc.map(tocItem => {
-        return {
-            label: tocItem.label,
-            click: () => {
-                mainWindow.webContents.send('toc-item-click', tocItem.href);
-            }
-        };
-    });
+  tocMenu = toc.map((tocItem) => {
+    return {
+      label: tocItem.label,
+      click: () => {
+        mainWindow.webContents.send('toc-item-click', tocItem.href);
+      },
+    };
+  });
 
-    const menu = Menu.buildFromTemplate(generateMenu());
-    Menu.setApplicationMenu(menu);
+  const menu = Menu.buildFromTemplate(generateMenu());
+  Menu.setApplicationMenu(menu);
 });
 ipcMain.on('open-file-dialog', (event) => {
-    dialog.showOpenDialog(mainWindow, {
-        properties: ['openFile'],
-        filters: [
-            {name: 'ePub Files', extensions: ['epub']}
-        ]
-    }).then(result => {
-        if (!result.canceled && result.filePaths.length > 0) {
-            // Handle the selected file, e.g., send it to the renderer process
-            event.sender.send('file-opened', result.filePaths[0]);
-        }
+  dialog
+    .showOpenDialog(mainWindow, {
+      properties: ['openFile'],
+      filters: [{ name: 'ePub Files', extensions: ['epub'] }],
+    })
+    .then((result) => {
+      if (!result.canceled && result.filePaths.length > 0) {
+        // Handle the selected file, e.g., send it to the renderer process
+        event.sender.send('file-opened', result.filePaths[0]);
+      }
     });
 });
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,105 +1,105 @@
-const {ipcRenderer} = require('electron')
-const ePub = require("epubjs").default
-const Store = require('electron-store');
-const store = new Store();
+const { ipcRenderer } = require('electron');
+const ePub = require('epubjs').default;
 
 let rendition;
 
 function updateTheme(isDarkMode) {
-    const linkColor = isDarkMode ? '#3b82f6' : '#3b82f6';
-    const trColor = isDarkMode ? '#252525' : '#F5F5F5';
-    const blockquoteBorderColor = isDarkMode ? '#454545' : '#DDDDDD';
-    const blockquoteBackgroundColor = isDarkMode ? '#252525' : '#F9F9F9';
-    rendition.themes.default({
-        'body': {
-            'font-family': 'Roboto, sans-serif',
-            'background-color': isDarkMode ? '#18181b' : 'white',
-            'color': isDarkMode ? '#e4e4e7' : 'black'
-        },
-        'pre, code': {
-            'font-family': 'JetBrains Mono, sans-serif',
-            'white-space': 'pre-wrap',
-            'word-wrap': 'break-word'
-        },
-        'a, a:link, a:visited': {
-            'color': linkColor
-        },
-        'tr': {
-            'background-color': trColor
-        },
-        'blockquote': {
-            'border-left': `4px solid ${blockquoteBorderColor}`,
-            'background-color': blockquoteBackgroundColor,
-            'padding': '10px 20px',
-            'margin': '20px 0'
-        }
-    });
+  const linkColor = isDarkMode ? '#3b82f6' : '#3b82f6';
+  const trColor = isDarkMode ? '#252525' : '#F5F5F5';
+  const blockquoteBorderColor = isDarkMode ? '#454545' : '#DDDDDD';
+  const blockquoteBackgroundColor = isDarkMode ? '#252525' : '#F9F9F9';
+  rendition.themes.default({
+    body: {
+      'font-family': 'Roboto, sans-serif',
+      'background-color': isDarkMode ? '#18181b' : 'white',
+      color: isDarkMode ? '#e4e4e7' : 'black',
+    },
+    'pre, code': {
+      'font-family': 'JetBrains Mono, sans-serif',
+      'white-space': 'pre-wrap',
+      'word-wrap': 'break-word',
+    },
+    'a, a:link, a:visited': {
+      color: linkColor,
+    },
+    tr: {
+      'background-color': trColor,
+    },
+    blockquote: {
+      'border-left': `4px solid ${blockquoteBorderColor}`,
+      'background-color': blockquoteBackgroundColor,
+      padding: '10px 20px',
+      margin: '20px 0',
+    },
+  });
 }
 
 ipcRenderer.on('file-opened', (event, file) => {
-    document.getElementById('splash-screen').style.display = 'none';
-    const book = ePub(file);
-    rendition = book.renderTo("viewer", {width: "100%", height: "100%"});
+  document.getElementById('splash-screen').style.display = 'none';
+  const book = ePub(file);
+  rendition = book.renderTo('viewer', { width: '100%', height: '100%' });
 
-    book.ready.then(() => {
-        const {toc} = book.navigation;
-        ipcRenderer.send('toc-ready', toc);
-        const lastLocation = store.get('lastKnownLocation');
-        if (lastLocation) {
-            rendition.display(lastLocation);
-        } else {
-            rendition.display();
-        }
-    });
+  book.ready.then(() => {
+    const { toc } = book.navigation;
+    ipcRenderer.send('toc-ready', toc);
+    const lastLocation = store.get('lastKnownLocation');
+    if (lastLocation) {
+      rendition.display(lastLocation);
+    } else {
+      rendition.display();
+    }
+  });
 
-    // When the book is ready and rendered, update the theme
-    rendition.on('rendered', () => {
-        updateTheme(document.documentElement.className === 'dark');
-    });
+  // When the book is ready and rendered, update the theme
+  rendition.on('rendered', () => {
+    updateTheme(document.documentElement.className === 'dark');
+  });
 
-    rendition.on('relocated', (location) => {
-        store.set('lastKnownLocation', location.start.cfi);
-    });
+  rendition.on('relocated', (location) => {
+    store.set('lastKnownLocation', location.start.cfi);
+  });
 });
 
 ipcRenderer.on('file-closed', () => {
-    if (rendition) {
-        rendition.destroy();
-        rendition = null;
-    }
-    document.getElementById('viewer').innerHTML = '';
+  if (rendition) {
+    rendition.destroy();
+    rendition = null;
+  }
+  document.getElementById('viewer').innerHTML = '';
 });
 
 ipcRenderer.on('app_version', (event, appVersion) => {
-    document.getElementById('app-version').innerText = `App version: ${appVersion}`;
+  document.getElementById(
+    'app-version'
+  ).innerText = `App version: ${appVersion}`;
 });
 
 ipcRenderer.on('toc-item-click', (event, href) => {
-    rendition.display(href);
+  rendition.display(href);
 });
 
 ipcRenderer.on('toggle-dark-mode', () => {
-    const isDarkMode = document.documentElement.className !== 'dark';
-    document.documentElement.className = isDarkMode ? 'dark' : '';
-    updateTheme(isDarkMode);
+  const isDarkMode = document.documentElement.className !== 'dark';
+  document.documentElement.className = isDarkMode ? 'dark' : '';
+  updateTheme(isDarkMode);
 });
 
 document.getElementById('next').addEventListener('click', () => {
-    rendition.next();
+  rendition.next();
 });
 
 document.getElementById('prev').addEventListener('click', () => {
-    rendition.prev();
+  rendition.prev();
 });
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-        rendition.next();
-    } else if (e.key === "ArrowLeft") {
-        rendition.prev();
-    }
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowRight') {
+    rendition.next();
+  } else if (e.key === 'ArrowLeft') {
+    rendition.prev();
+  }
 });
 
 document.getElementById('open-file-button').addEventListener('click', () => {
-    ipcRenderer.send('open-file-dialog');
+  ipcRenderer.send('open-file-dialog');
 });
